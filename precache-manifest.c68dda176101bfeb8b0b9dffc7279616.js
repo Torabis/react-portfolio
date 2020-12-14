535fe47c9b9abce4420ef2188266421234feed5e
Torabis <sadihome@gmail.com>
2020-12-13 18:22:35 -0800

    Updates

---
+++ precache-manifest.c68dda176101bfeb8b0b9dffc7279616.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "910d6945e93f486c2f985f3cc0ddc171",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "91217595105c4374d649",
+    "url": "/react-portfolio/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "7cb5518a9e50630ccb3b",
+    "url": "/react-portfolio/static/css/main.de372734.chunk.css"
+  },
+  {
+    "revision": "91217595105c4374d649",
+    "url": "/react-portfolio/static/js/2.018a47e7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-portfolio/static/js/2.018a47e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cb5518a9e50630ccb3b",
+    "url": "/react-portfolio/static/js/main.bf132fe0.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "eb8b9f54e7c50c6371c642e46960e38e",
+    "url": "/react-portfolio/static/media/logo.eb8b9f54.png"
+  },
+  {
+    "revision": "b25a8b2a2a3771ed40fd563de703f811",
+    "url": "/react-portfolio/static/media/profilepic.b25a8b2a.png"
+  }
+]);
